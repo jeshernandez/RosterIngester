19f4acb2a24daad1f4e493bc545422dc8f94d449
Jesse Hernandez <futcomker@gmail.com>
2017-07-19 09:18:20 -0700

    Added USPS as an option to Address Engine. .

--- src/main/java/com/rosteringester/usps/AddressEngine.java
+++ src/main/java/com/rosteringester/usps/AddressEngine.java
@@ -32,6 +32,84 @@ public class AddressEngine {
     }
 
 
+    public void startUSPS(String queryFile, String updateQuery) {
+
+
+        DbDB2 db = new DbDB2();
+
+        // Get DB2 Connection
+        Connection conn;
+        conn = db.getDBConn();
+
+        // Get query File
+        String query = new ReadEntireTextFiles()
+                .getTextData(this.directoryPath + "\\" + queryFile);
+        // Send the query
+        db.query(conn, query);
+
+
+        USPS s = new USPS();
+
+
+        int rowCount = db.getRowCount();
+        System.out.println("Row counts: " + rowCount);
+
+
+
+        // Get update query  File
+        String updateFile = new ReadEntireTextFiles()
+                .getTextData(this.directoryPath + "\\" + updateQuery);
+
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
+
+        for (int i = 0; i < rowCount; i++){
+
+            System.out.println("Processing: " + i + " , of " + rowCount);
+            System.out.println("Sending Address: " + db.getValueAt(i, 0).toString());
+            String[] address = {db.getValueAt(i, 0).toString()};
+            String[] city = {db.getValueAt(i, 1).toString()};
+            String[] state = {db.getValueAt(i, 2).toString()};
+
+            s.start(true, address, city, state);
+            System.out.println("USPS Address: " + s.getValueAt(0, 0).toString());
+
+            try {
+                PreparedStatement pstmt = conn.prepareStatement(updateFile);
+                pstmt.setString(1,  s.getValueAt(0, 0).toString());
+                String todaysDate = sdf.format(cal.getTime());
+                pstmt.setString(2, todaysDate);
+                pstmt.setString(3, System.getProperty("user.name").toUpperCase());
+                pstmt.setString(4, address[0]);
+                pstmt.setString(5, city[0]);
+                pstmt.setString(6, state[0]);
+                pstmt.addBatch();
+                pstmt.executeBatch();
+                pstmt.close();
+                conn.commit();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
+
+        // Close the connection if its open.
+        try {
+            if(!conn.isClosed()) {
+                System.out.println("Not closed, closing...");
+                conn.close();
+            }
+
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    } // End of startStandard Method
+
+
     // ----------------------------------------------
     public void startStandard(String queryFile, String updateQuery) {
 

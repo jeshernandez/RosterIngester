25e62a96f236de4c62feea9ad740715810f52c80
Michael Chrisco <michaelachrisco@gmail.com>
2017-07-21 19:02:43 -0400

    Basic logging occurs on grips_log_error table.

--- src/main/java/com/rosteringester/main/RosterIngester.java
+++ src/main/java/com/rosteringester/main/RosterIngester.java
@@ -15,9 +15,7 @@ public class RosterIngester {
     public static boolean debug = false;
     // TODO me - 07/04/2017 remove word from key and add to requiredFields
     // TODO me - 07/04/2017 find a way to remove highest score for iterator
-
    //TODO: Michael - Add into its own Service Object. Remove from main method.
-
 
     public static void main(String [] args) {
 
@@ -36,8 +34,5 @@ public class RosterIngester {
 
 
     } // End of Main
-
-
-
 
 } // End of RosterIngester

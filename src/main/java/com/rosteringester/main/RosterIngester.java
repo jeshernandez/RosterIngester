9f6e9b92cc2894029bc96d90c15af4b1771cb5dd
Michael Chrisco <michaelachrisco@gmail.com>
2017-07-21 14:56:07 -0400

    Working Logging on console.

--- src/main/java/com/rosteringester/main/RosterIngester.java
+++ src/main/java/com/rosteringester/main/RosterIngester.java
@@ -1,6 +1,5 @@
 package com.rosteringester.main;
 
-
 import com.rosteringester.roster.Discovery;
 import com.rosteringester.roster.Roster;
 import com.rosteringester.roster.RosterFactory;
@@ -8,11 +7,11 @@ import com.rosteringester.roster.RosterFactory;
 import java.util.List;
 import java.util.Map;
 
-
 /**
  * Created by jeshernandez on 6/14/17.
  */
 public class RosterIngester {
+    private static final Logger logger = LogManager.getLogger(RosterIngester.class.getName());
     public static boolean debug = false;
     // TODO me - 07/04/2017 remove word from key and add to requiredFields
     // TODO me - 07/04/2017 find a way to remove highest score for iterator
@@ -37,7 +36,6 @@ public class RosterIngester {
 
 
     } // End of Main
-
 
 
 

90cc6a3859d8c8536ffb1bee48049de3e022abb4
Michael Chrisco <michaelachrisco@gmail.com>
2017-07-22 14:42:42 -0400

    Attept to unit test LogQueryExceptionBuilder. Issue with mocking a throwable Iteration. Will have to revisit.

--- src/tests/java/com/rosteringester/logs/LogQueryErrorTest.java
+++ src/tests/java/com/rosteringester/logs/LogQueryErrorTest.java
@@ -4,10 +4,14 @@ import com.microsoft.sqlserver.jdbc.SQLServerDriver;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.PrintStream;
 import java.sql.SQLException;
 import com.rosteringester.logs.LogQueryErrorExceptionBuilder;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 
 /**
  * Created by Michael Chrisco on 07/22/2017.
@@ -16,7 +20,16 @@ public class LogQueryErrorTest {
     @Test
     public void exceptionBuilder() throws SQLException {
         //TODO: Mock SQLException model and test.
-        LogQueryError subject = LogQueryError.ExceptionBuilder(new SQLException(), this).build();
+        //Throwable is causing issues with this unit test. Will revisit.
+        SQLException test = Mockito.mock(SQLException.class);
+        SQLException testCompare = Mockito.mock(SQLException.class);
+        Mockito.when(test.getSQLState()).thenReturn("State");
+        Mockito.when(test.getErrorCode()).thenReturn(999);
+        Mockito.when(test.getMessage()).thenReturn("message");
+        testCompare.getStackTrace();
+//        testCompare.getCause(test);
+//        Mockito.when(test.printStackTrace(new PrintStream()));
+        LogQueryError subject = LogQueryError.ExceptionBuilder(testCompare, this).build();
         assertFalse(subject.getSavedFlag());
 //        assertEquals("String", subject.getStrClass());
         assertNull(subject.getState());
@@ -27,5 +40,4 @@ public class LogQueryErrorTest {
     @Test
     public void create() throws Exception {
     }
-
 }

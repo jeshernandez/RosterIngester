ee833ce04caa690dbc3635a96f183a19cabcc387
Michael Chrisco <michaelachrisco@gmail.com>
2017-07-23 14:59:16 -0400

    Need to investigate if bulk save can occur on Roster object.

--- src/main/java/com/rosteringester/db/dbModels/DBRosterBulkSave.java
+++ src/main/java/com/rosteringester/db/dbModels/DBRosterBulkSave.java
@@ -1,9 +1,16 @@
 package com.rosteringester.db.dbModels;
 
+import com.rosteringester.roster.Roster;
+
 /**
  * Created by Michael Chrisco on 07/22/2017.
  * Takes the DBRosterCollection and saves them in bulk.
  * Default is 200 records at a time.
  */
 public class DBRosterBulkSave {
+    private String query = "INSERT into [dbo].[grips_roster_required] (npi, address, suite, city, zip, state) values (?, ?, ?, ?, ?, ?)";
+
+    public DBRosterBulkSave(Roster roster){
+
+    }
 }

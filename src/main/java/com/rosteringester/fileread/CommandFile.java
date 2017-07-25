1e266f227f9adcced8be651235fa80676d969d44
Michael Chrisco <michaelachrisco@gmail.com>
2017-07-24 21:00:05 -0400

    Change command to only include fallout vs no fallout files.

--- src/main/java/com/rosteringester/fileread/CommandFile.java
+++ src/main/java/com/rosteringester/fileread/CommandFile.java
@@ -11,47 +11,57 @@ import java.nio.file.Paths;
  * Created by Michael Chrisco on 07/24/2017.
  * The Command File Strategy categorizes files based on their file name via the CategorizeFileStrategy.
  * It sends the file to be ingested or pushes it to an error folder for Accenture/someone to set as standard.
+ * Usage:
+ * CommandFile commandFile = new CommandFile("C:/DATA/roster/cov_roster.xslx")
+ * commandFile.setFileName("C:/DATA/roster/cov_roster.xslx").categorize();
+ * if(commandFile.isFallout()){
+ *     commandFile.sendFalloutFile();
+ *     //stop processing this file
+ * }
+ * else...continue.
  */
 public class CommandFile {
     private String fileName;
     private int categorization;
 
+    /**
+     * Command sets the file name from an absolute path.
+     * @param fileName String filename
+     * @return self
+     */
     public CommandFile setFileName(String fileName){
         this.fileName = fileName;
         return this;
     }
 
+    /**
+     * Command categorizes via the CategorizeFileStrategy
+     * @return
+     */
     public CommandFile categorize(){
         this.categorization = (new CategorizeFileStrategy(fileName)).getCategorization();
         return this;
     }
 
+    /**
+     * True if the file is in error and is part of the fallout else false.
+     * @return boolean
+     */
+    public Boolean isFallout(){
+        return (categorization != -1);
+    }
+
     //This should be done in another strategy or other object.
-    public CommandFile sendFile(){
-        String target = "";
-        if(categorization == 0){
-            target = "coventry/";
-        }
-        if(categorization == 1){
-            target = "aetna/";
-        }
-        if(categorization == 2){
-            target = "both/";
-        }
-        if(categorization == -1){
-            target = "error/";
-        }
-        //TODO: Double check on path and file. If path is not correct, catch the error.
+    /**
+     * Moves the fallout file to the fallout folder.
+     */
+    public void sendFalloutFile(){
         //TODO: Add in Path into env file? Or start hard setting on the server.
         try {
-            File slicedFileName = new File(fileName);
-            System.out.println("OrigionalFile: "+ Paths.get(fileName));
-            System.out.println("MovedFile: " + "C:/DATA/rosters/" + target + slicedFileName.getName());
-            Files.move(Paths.get(fileName), Paths.get("C:/DATA/rosters/" + target + slicedFileName.getName()));
+            String slicedFileName = (new File(fileName)).getName();
+            Files.move(Paths.get(fileName), Paths.get("C:/DATA/rosters/fallout" + slicedFileName));
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        return this;
     }
 }

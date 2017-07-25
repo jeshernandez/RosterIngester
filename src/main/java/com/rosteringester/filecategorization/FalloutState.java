1540708b9898b4e6f43ade75ffbdb95cf4b6ea8f
Michael Chrisco <michaelachrisco@gmail.com>
2017-07-25 16:16:29 -0400

    Reuse state creation of file directories via CreateFileDirectory.

--- src/main/java/com/rosteringester/filecategorization/FalloutState.java
+++ src/main/java/com/rosteringester/filecategorization/FalloutState.java
@@ -16,13 +16,13 @@ import java.util.logging.Logger;
  * Created by Michael Chrisco on 07/24/2017.
  * Handles the fallout state of a file by sending the file to error_<yyyy_mm_dd)/<file>
  */
-public class FalloutState implements State {
+public class FalloutState extends CreateFileDirectory implements State  {
     Logger log = Logger.getLogger(FileState.class.getName());
 
     public Boolean handle(String fileName){
         try {
             String slicedFileName = (new File(fileName)).getName();
-            String sendPath = createDirectory();
+            String sendPath = CreateFileDirectory.createDirectory("error");
             Files.move(Paths.get(fileName), Paths.get(sendPath + slicedFileName));
             log.info("FalloutState: moved " + fileName + " to " + sendPath + slicedFileName);
         } catch (IOException e) {
@@ -30,21 +30,5 @@ public class FalloutState implements State {
         }
         log.info("FalloutState: handled " + fileName);
         return true;
-    }
-
-    private String createDirectory(){
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy_MM_dd");
-        LocalDate localDate = LocalDate.now();
-        String date = dtf.format(localDate);
-        String sendPath = "C:/DATA/rosters/error_" + date + "/";
-        File file = new File(sendPath);
-        if (!file.exists()) {
-            if (file.mkdir()) {
-                log.info("Created Directory " + sendPath);
-            } else {
-                log.warning("Failed to create directory " + sendPath);
-            }
-        }
-        return file.getAbsolutePath() + "/";
     }
 }
--- src/main/java/com/rosteringester/filecategorization/FileState.java
+++ src/main/java/com/rosteringester/filecategorization/FileState.java
@@ -12,13 +12,13 @@ import java.util.logging.Logger;
  * Created by Michael Chrisco on 07/24/2017.
  * Handles the state by pushing successful files to success_<yyyy_mm_dd)/<file>
  */
-public class FileState implements State {
+public class FileState extends CreateFileDirectory implements State {
     Logger log = Logger.getLogger(FileState.class.getName());
 
     public Boolean handle(String fileName){
         try {
             String slicedFileName = (new File(fileName)).getName();
-            String sendPath = createDirectory();
+            String sendPath = CreateFileDirectory.createDirectory("success");
             Files.move(Paths.get(fileName), Paths.get(sendPath + slicedFileName));
             log.info("FalloutState: moved " + fileName + " to " + sendPath + slicedFileName);
         } catch (IOException e) {
@@ -26,21 +26,5 @@ public class FileState implements State {
         }
         log.info("FileState: handled " + fileName);
         return true;
-    }
-
-    private String createDirectory(){
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy_MM_dd");
-        LocalDate localDate = LocalDate.now();
-        String date = dtf.format(localDate);
-        String sendPath = "C:/DATA/rosters/success_" + date + "/";
-        File file = new File(sendPath);
-        if (!file.exists()) {
-            if (file.mkdir()) {
-                log.info("Created Directory " + sendPath);
-            } else {
-                log.warning("Failed to create directory " + sendPath);
-            }
-        }
-        return file.getAbsolutePath() + "/";
     }
 }

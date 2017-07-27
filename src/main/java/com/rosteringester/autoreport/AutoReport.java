282df1a4b3d8b92809f1e04e871574c153e6c29a
Jesse Hernandez <futcomker@gmail.com>
2017-07-27 15:20:58 -0700

    Refactored to increase abstract usage.

--- src/main/java/com/rosteringester/autoreport/AutoReport.java
+++ src/main/java/com/rosteringester/autoreport/AutoReport.java
@@ -3,6 +3,7 @@ package com.rosteringester.autoreport;
 import com.rosteringester.db.DbSqlServer;
 import com.rosteringester.fileread.FileFactory;
 import com.rosteringester.filewrite.XLSXWriter;
+import com.rosteringester.logs.LogReportCreation;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.sql.Connection;
@@ -14,26 +15,33 @@ import java.util.Vector;
 /**
  * Created by jeshernandez on 07/26/2017.
  */
-public class AutoReport {
+public class AutoReport extends ReportP {
     private String directoryOutput = "C:\\DATA\\BATCH\\PICKUP_FILES\\";
     private String fileName = "EPDB_NotInRoster_Delegate_";
+    private String reportName = "EPDB_Not_InRoster";
+    private String delegatedFileLoc = "\"C:/Data/delegates.xlsx\"";
 
     // TODO - Create factory to select specific report based on injected value.
+    // TODO - Create a way to not embed tab names and dynamically send new tab objects.
 
     public void start() {
 
-    FileFactory f = new FileFactory("C:/Data/delegates.xlsx");
+    FileFactory f = new FileFactory(delegatedFileLoc);
+
     String[] DELEGATES = new String[300];
     CaptureXLSX rxls = new CaptureXLSX();
     DELEGATES = rxls.start();
 
+    System.out.println("Report Name: " + reportName);
 
     DbSqlServer sql = new DbSqlServer();
         sql.setConnectionUrl();
 
     Connection conn;
     conn = sql.getDBConn();
+    LogReportCreation dbLog = null;
 
+    // DELEGATES.length
         for (int i = 1; i < DELEGATES.length; i++) {
         // Open the master workbook for report
         XSSFWorkbook wb = new XSSFWorkbook();
@@ -42,9 +50,10 @@ public class AutoReport {
         // Send first sheet data values
         String[] headers = sql.getHeaders();
         XLSXWriter x = new XLSXWriter();
-        x.createExcelFile("EPDB_Not_InRoster", directoryOutput + fileName
+        x.createExcelFile(reportName, directoryOutput + fileName
                         + DELEGATES[i] + "_" + getFileDate() + ".xlsx",
                 headers, data, wb, true);
+
 
         // Run second query for second sheet
         Vector<String[]> falloutData = sql.stAddressFallout(conn, DELEGATES[i].toString());
@@ -53,6 +62,14 @@ public class AutoReport {
         x.createExcelFile("AddressFallOut", directoryOutput + fileName
                         + DELEGATES[i] + "_" + getFileDate() + ".xlsx",
                 falloutHeaders, falloutData, wb, false);
+            dbLog = new LogReportCreation.Builder()
+                    .reportName(reportName)
+                    .delegateId(Integer.parseInt(DELEGATES[i].toString()))
+                    .epdbTotal(data.size())
+                    .uspsFallout(falloutData.size())
+                    .dateCreated(dbDate())
+                    .build()
+                    .create(conn);
 
 
     }
@@ -67,14 +84,6 @@ public class AutoReport {
     } // end of start method
 
 
-    // ----------------------------------------------------------------------------
-    public static String getFileDate() {
-        Calendar cal = Calendar.getInstance();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
-        String todaysDate = sdf.format(cal.getTime());
-
-        return todaysDate;
-    }
 
 
 } // End of AutoReport class

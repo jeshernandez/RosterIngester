3105c5c95597b9cbd73385c64dcb5198bc277930
Jesse Hernandez <futcomker@gmail.com>
2017-08-07 02:56:35 -0700

    Main launch.

--- src/main/java/com/rosteringester/main/RosterIngester.java
+++ src/main/java/com/rosteringester/main/RosterIngester.java
@@ -1,5 +1,9 @@
 package com.rosteringester.main;
 
+import com.rosteringester.algorithms.AlgoFactory;
+import com.rosteringester.autoreport.AutoReport;
+import com.rosteringester.discovery.DiscoverMedicare;
+import com.rosteringester.filecategorization.CategorizeFileStrategy;
 import com.rosteringester.filecategorization.DirectoryFileCategorization;
 import com.rosteringester.fileread.DirectoryFiles;
 import com.rosteringester.fileread.FileFactory;
@@ -27,16 +31,61 @@ public class RosterIngester {
 
 
     public static void main(String [] args) {
-        DirectoryFileCategorization directory = new DirectoryFileCategorization();
-        List files = directory.categorizeDirectoryFiles();
 
-        //Get file Categories. Will be used later.
-        HashMap<String, Integer> fileCategories = directory.getFileCategories();
-//        System.out.println(fileCategories.toString());
-        DirectoryFiles directoryFiles = new DirectoryFiles();
+        // IDEAL SOLUTION
+        // ------------------------------
+        // step 1 -> file category
+        // step 2 -> file reading *
+        // step 3 -> algorithm *
+        // step 4 -> database (ingest) ??
+        // step 5 -> filewrite (output normalized roster)
+        // step 6 -> usps (standardize address)
+        // step 7 -> business rules for RPDB compare
+        // step 8 -> autoreport (output to network drive).
 
-        DiscoverFields d = new DiscoverFields();
-        d.findField();
+        // PLAN B
+        // -------------
+        // step 1 -> Donna to label every file
+        // step 2 -> Accenture standardizes rosters
+        // step 3 -> Ingest with Alteryx
+        // step 4 -> usps (standardize address)
+        // step 5 -> business rules for RPDB compare
+        // step 6 -> autoreport (output to network drive).
+
+
+        DiscoverMedicare medicare = new DiscoverMedicare();
+        medicare.findField();
+
+//        AlgoFactory f = new AlgoFactory();
+//        Double d = 0.0;
+//        String cleanfield = "accepting new patient";
+//        String[] dictionary = {"tax id number", "tax id", "tin", "provider tin number", "provider tin",
+//                "individual tin", "taxid", "practice tin"};
+//        d = f.getBestScore("j", cleanfield, dictionary);
+
+//        String[] dictionary1 = new String[10];
+//        dictionary1[0] = "tax id number"; // .48 // NO
+//        dictionary1[1] = "tax id"; // .45 // NO
+//        dictionary1[2] = "tin"; // .71 , YES
+//        dictionary1[3] = "provider tin number"; // .53, NO
+//        dictionary1[4] = "provider tin"; // .5 , NO
+//        dictionary1[5] = "individual tin"; // 0.47, NO
+//        dictionary1[6] = "taxid"; // 0.47, NO
+//        dictionary1[7] = "practice tin"; //0.66, NO
+//
+//        d = f.getScore("j", "accepting new patient", dictionary1[7]);
+//
+//        if(cleanfield.contains(dictionary1[7])) {
+//            System.out.println("Contains is true");
+//        }
+
+//        System.out.println("Algo Test: " + d);
+
+//        DirectoryFileCategorization dfc = new DirectoryFileCategorization();
+//        dfc.categorizeDirectoryFiles();
+
+//        AutoReport r = new AutoReport();
+//        r.start();
 
 
 

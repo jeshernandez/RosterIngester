98abb414889ee8be5b1352f7f00429d9b2842e26
Jesse Hernandez <futcomker@gmail.com>
2017-08-12 16:26:29 -0700

    Now can read blank and null lines.

--- src/main/java/com/rosteringester/fileread/XLSXFile.java
+++ src/main/java/com/rosteringester/fileread/XLSXFile.java
@@ -71,45 +71,59 @@ public class XLSXFile extends Excel implements FileReader {
             XSSFSheet sheet = wb.getSheetAt(0);
             sheet.setDisplayGridlines(false);
 
-            int getTotalRecords = sheet.getPhysicalNumberOfRows()+1;
+            int rowCount = sheet.getPhysicalNumberOfRows()+1;
 
             XSSFRow rows = sheet.getRow(0);
             int columnCount = rows.getLastCellNum();
             System.out.println("Column Count: " + columnCount);
-            System.out.println("Row Count: " + getTotalRecords);
-            xlsxData = new String[getTotalRecords][columnCount];
+            System.out.println("Row Count: " + rowCount);
+            xlsxData = new String[rowCount][columnCount];
 
             int rowTracker = 0;
             int colTracker = 0;
 
             SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy");
 
+            //System.out.println("::: READING ROW COUNT: " + rowCount);
+            //System.out.println("::: READING COL COUNT: " + columnCount);
 
-            for (Row row : sheet) {
-                for (Cell cell : row) {
-                        cell.getCellTypeEnum();
+            //for (Row row : sheet) {
+              for (int r = 0; r < rowCount-1; r++) {
+                  Row row = sheet.getRow(r);
 
-                        if(cell.getCellTypeEnum().equals(CellType.STRING)) {
-                            xlsxData[rowTracker][colTracker] = cell.getStringCellValue();
-                            //System.out.println("[STRING]: " + xlsxData[rowTracker][colTracker]);
-                        } else if (cell.getCellTypeEnum().equals(CellType.NUMERIC)) {
-                            if (DateUtil.isCellDateFormatted(cell)) {
-                                xlsxData[rowTracker][colTracker] = df.format(cell.getDateCellValue());
-                                //System.out.println("[DATE]: " + df.format(cell.getDateCellValue()));
-                            } else {
-                                xlsxData[rowTracker][colTracker] = String.valueOf((int) cell.getNumericCellValue()).toString();
-                                //System.out.println("[NUMERIC]: " + xlsxData[rowTracker][colTracker]);
-                            }
+                //for (Cell cell : row) {
+                  for (int c = 0; c < columnCount; c++) {
+                        Cell cell = row.getCell(c);
 
-                        } else if (cell.getCellTypeEnum().equals(CellType.ERROR)) {
-                            xlsxData[rowTracker][colTracker] = "error";
-                        } else if (cell.getCellTypeEnum().equals(CellType._NONE)) {
-                            xlsxData[rowTracker][colTracker] = "none";
-                        }  else if (cell.getCellTypeEnum().equals(CellType.FORMULA)) {
-                            xlsxData[rowTracker][colTracker] = "formula";
-                        } else if (cell.getCellTypeEnum().equals(CellType.BLANK)) {
+                        if(cell == null) {
                             xlsxData[rowTracker][colTracker] = "";
-                            //System.out.println("[BLANK]: " + xlsxData[rowTracker][colTracker]);
+                            //System.out.println("[NULL]: " + xlsxData[rowTracker][colTracker]);
+                        } else {
+
+                            cell.getCellTypeEnum();
+
+                            if(cell.getCellTypeEnum().equals(CellType.STRING)) {
+                                xlsxData[rowTracker][colTracker] = cell.getStringCellValue();
+                                //System.out.println("[STRING]: " + xlsxData[rowTracker][colTracker]);
+                            } else if (cell.getCellTypeEnum().equals(CellType.NUMERIC)) {
+                                if (DateUtil.isCellDateFormatted(cell)) {
+                                    xlsxData[rowTracker][colTracker] = df.format(cell.getDateCellValue());
+                                    //System.out.println("[DATE]: " + df.format(cell.getDateCellValue()));
+                                } else {
+                                    xlsxData[rowTracker][colTracker] = String.valueOf((int) cell.getNumericCellValue()).toString();
+                                    //System.out.println("[NUMERIC]: " + xlsxData[rowTracker][colTracker]);
+                                }
+
+                            } else if (cell.getCellTypeEnum().equals(CellType.ERROR)) {
+                                xlsxData[rowTracker][colTracker] = "error";
+                            } else if (cell.getCellTypeEnum().equals(CellType._NONE)) {
+                                xlsxData[rowTracker][colTracker] = "none";
+                            }  else if (cell.getCellTypeEnum().equals(CellType.FORMULA)) {
+                                xlsxData[rowTracker][colTracker] = "formula";
+                            } else if (cell.getCellTypeEnum().equals(CellType.BLANK)) {
+                                xlsxData[rowTracker][colTracker] = "";
+                                //System.out.println("[BLANK]: " + xlsxData[rowTracker][colTracker]);
+                            }
                         }
 
                     colTracker++;

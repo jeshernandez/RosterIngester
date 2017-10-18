879a7758ee3ecb22f86950ac63c5382859ca43c2
Jesse Hernandez <futcomker@gmail.com>
2017-10-18 09:40:55 -0700

    synchronizing last code.

--- src/main/java/com/rosteringester/usps/AddressEngine.java
+++ src/main/java/com/rosteringester/usps/AddressEngine.java
@@ -259,11 +259,166 @@ public class AddressEngine {
         }
 
 
+    } // End of startStandard Method
+
+
+
+    // ------------SMARTY STREETS-----------------------
+    public void startSmartyWithSuite(String queryFile, String updateQuery) {
+
+
+        //DbDB2 db = new DbDB2();
+        DbSqlServer db = new DbSqlServer();
+        db.setConnectionUrl();
+
+        // Get DB2 Connection
+        Connection conn;
+        conn = db.getDBConn();
+
+        // Get query File
+        String query = new ReadEntireTextFiles()
+                .getTextData(this.directoryPath + "\\" + queryFile);
+        // Send the query
+        db.query(conn, query);
+
+
+        //Run Smarty first.
+        SmartyStreets s;
+
+        // Run USPS Last
+        // Update query to only query those "Invalid Address" for USPS_ADDRESS
+        //USPS s = new USPS();
+
+        int rowCount = db.getRowCount();
+        if(localDebug) System.out.println("Row counts: " + rowCount);
+
+
+        // Check to see if rowCount is smaller than batchMax.
+        // this allows for address under batchMax to be processed.
+        if(rowCount < batchMax) batchMax = rowCount-1;
+
+        // Get update query  File
+        String updateFile = new ReadEntireTextFiles()
+                .getTextData(this.directoryPath + "\\" + updateQuery);
+
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
+
+
+
+        int[] id = new int[batchMax];
+        String[] address = new String[batchMax];
+        String[] suite = new String[batchMax];
+        String[] addressSuite = new String[batchMax];
+        String[] city = new String[batchMax];
+        String[] state = new String[batchMax];
+        int batchHolder = 0;
+        int statementHolder = 0;
+        PreparedStatement pstmt = null;
+
+
+        try {
+            pstmt  = conn.prepareStatement(updateFile);
+
+            for (int i = 0; i < rowCount; i++) {
+
+                if(statementHolder < batchMax ) {
+                    if (localDebug) System.out.println("Processing: " + i + " , of " + rowCount);
+                    if (localDebug) System.out.println("Batching Address[" + db.getValueAt(i, 1).toString()
+                            + "] Suite[" + db.getValueAt(i, 2).toString()
+                            + "] City[" + db.getValueAt(i, 3).toString() + "]");
+                    id[statementHolder] = Integer.parseInt(db.getValueAt(i, 0).toString());
+                    String addressWithSuite = db.getValueAt(i, 1).toString() + " " + db.getValueAt(i, 2).toString();
+                    suite[statementHolder] = db.getValueAt(i, 2).toString();
+                    address[statementHolder] = db.getValueAt(i, 1).toString();
+                    addressSuite[statementHolder] = addressWithSuite;
+                    city[statementHolder] = db.getValueAt(i, 3).toString();
+                    state[statementHolder] = db.getValueAt(i, 4).toString();
+
+                    statementHolder++;
+                } else {
+                    System.out.println("Sending to smarty streets...");
+                    s = new SmartyStreets();
+                    s.startSuite(true, addressSuite, city, state);
+
+                    // Adding 1 to statementHolder to activate commit.
+                    for (int b = 0; b < statementHolder+1; b++) {
+                        if(batchHolder != statementHolder) {
+                            if (localDebug) System.out.println("Adding SQL Statement, ID [" + id[batchHolder] + " ["
+                                    + s.getValueAt(batchHolder, 0).toString() + "]");
+
+
+                            // Invalid address workaround
+                            if(s.getValueAt(batchHolder, 1) == null) {
+                                pstmt.setString(1, s.getValueAt(batchHolder, 0).toString()); // full usps_address
+                                pstmt.setString(2, ""); // usps suite
+                                pstmt.setString(3, ""); // usps city
+                                pstmt.setString(4, ""); // usps state
+                                pstmt.setString(5, ""); // usps zip
+                                pstmt.setString(6, ""); // address active?
+                                pstmt.setString(7, ""); // address vacant?
+                                pstmt.setString(8, ""); // county
+                            } else {
+                                // complete update
+                                pstmt.setString(1, s.getValueAt(batchHolder, 0).toString()); // full usps_address
+                                pstmt.setString(2, s.getValueAt(batchHolder, 1).toString()); // usps suite
+                                pstmt.setString(3, s.getValueAt(batchHolder, 2).toString()); // usps city
+                                pstmt.setString(4, s.getValueAt(batchHolder, 3).toString()); // usps state
+                                pstmt.setString(5, s.getValueAt(batchHolder, 4).toString()); // usps zip
+                                pstmt.setString(6, s.getValueAt(batchHolder, 5).toString()); // address active?
+                                pstmt.setString(7, s.getValueAt(batchHolder, 6).toString()); // address vacant?
+                                pstmt.setString(8, s.getValueAt(batchHolder, 7).toString().toUpperCase()); // county
+                            }
+
+
+                            String todaysDate = sdf.format(cal.getTime());
+                            pstmt.setString(9, todaysDate);
+                            pstmt.setString(10, System.getProperty("user.name").toUpperCase());
+                            pstmt.setInt(11, id[batchHolder]);
+                            pstmt.addBatch();
+                            batchHolder++;
+                        } else {
+                            pstmt.executeBatch();
+                            pstmt.close();
+                            conn.commit();
+                            if(localDebug) System.out.println("Database update successful...");
+                        }
+                    } // end for-loop
+                    // Reset everything.
+                    batchHolder = 0;
+                    statementHolder = 0;
+                    address = new String[batchMax];
+                    addressSuite = new String[batchMax];
+                    city = new String[batchMax];
+                    state = new String[batchMax];
+                    pstmt = conn.prepareStatement(updateFile);
+
+                } // End first if-else
+
+            } // End for-loop
+
+            // Wait a few seconds before closing.
+            Thread.sleep(4000);
+
+            if(!pstmt.isClosed()) pstmt.close();
+            // Close the connection if its open.
+            if(!conn.isClosed()) {
+                LOGGER.info("Address Engine connection closing...");
+                conn.close();
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e1) {
+            e1.printStackTrace();
+        }
+
+
 
 
 
     } // End of startStandard Method
-
 
 
 
